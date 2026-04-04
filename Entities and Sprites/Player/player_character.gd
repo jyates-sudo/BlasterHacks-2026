95be0aafafecc455dcbe89f0859b96027babb76b
zAtkinsSchool <zatkins1@msudenver.edu>
2026-04-04 12:39:47 -0600

    ok this time it will work trust

---
+++ Entities and Sprites/Player/player_character.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export_category("Player Stats")
+@export var MoveSpeed = 300.0
+
+var move_direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	Player_base_movement()
+
+func Player_base_movement() -> void:
+	move_direction.x = int(Input.is_action_pressed("Player_move_left")) - int(Input.is_action_pressed("Player_move_right"))
+	move_direction.y = int(Input.is_action_pressed("Player_move_up")) - int(Input.is_action_pressed("Player_move_down"))
+	var motion: Vector2 = move_direction.normalized() * MoveSpeed
+	set_velocity(motion)
+	move_and_slide()
